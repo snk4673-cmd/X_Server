db73e225206bc8a0b4be47f324702a5023b12054
[snk4673-cmd] <snk4673@gmail.com>
2025-12-02 16:08:56 +0900

    dlr

--- controller/auth.mjs
+++ controller/auth.mjs
@@ -45,7 +45,7 @@ export async function login(req, res, next) {
   }
 
   const token = await createJwtToken(user.id);
-  res.status(200).json({ token, user });
+  res.status(200).json({ token, userid });
 }
 
 export async function me(req, res, next) {
